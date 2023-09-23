95164ec7492e439c1c30b033eb657c52d1b639aa
diptimalla <pdaffy0@gmail.com>
2023-09-23 20:11:45 +0530

    assign

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly rwOnlyObj = new RWOnly();
+        rwOnlyObj.setName("Dipti");
+
+
+        String retrievedName = rwOnlyObj.getName();
+        System.out.println("Name: " + retrievedName);
+    }
   
 }
